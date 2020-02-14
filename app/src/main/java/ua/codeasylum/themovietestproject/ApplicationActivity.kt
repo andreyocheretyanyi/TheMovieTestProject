2a4f7c9c9961a80051d95581f58fa8465b68388c
Andrey <andrey.ocheretyanyi@gmail.com>
2020-02-14 21:45:32 +0200

    added some business logic to repos

--- app/src/main/java/ua/codeasylum/themovietestproject/ApplicationActivity.kt
+++ app/src/main/java/ua/codeasylum/themovietestproject/ApplicationActivity.kt
@@ -1,12 +1,22 @@
 package ua.codeasylum.themovietestproject
 
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ua.codeasylum.themovietestproject.base.BaseActivity
 
 class ApplicationActivity : BaseActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application)
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        this.findNavController(R.id.nav_host_fragment).navigateUp()
+
+
 }
