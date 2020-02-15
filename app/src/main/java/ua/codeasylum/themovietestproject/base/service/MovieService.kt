a204f9b1cd9b4b30d40d2a8b90bf6b7af19855ee
Andrey <andrey.ocheretyanyi@gmail.com>
2020-02-15 17:30:23 +0200

    made result fragment

--- app/src/main/java/ua/codeasylum/themovietestproject/base/service/MovieService.kt
+++ app/src/main/java/ua/codeasylum/themovietestproject/base/service/MovieService.kt
@@ -10,7 +10,7 @@ interface MovieService {
     @GET("discover/movie")
     fun discoverMovie(
         @Query("api_key") apiKey: String, @Query("include_adult") includeAdult: Boolean,
-        @Query("page") page: Int, @Query("year") year: Int?, @Query("with_genres") genres: String,
+        @Query("page") page: Int, @Query("primary_release_year") year: Int?, @Query("with_genres") genres: String,
         @Query("with_people") people: String
     ) : Single<MovieDto>
 
